6c9f67ec7519e5ffb408a77dc7044f1395c7dcb3
Khodani <Khodani.mulisha@halocare.co.za>
2022-06-08 10:03:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnverSOft_CSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = ConfigurationManager.AppSettings["File"].ToString();
+
+
+            using (var reader = new StreamReader(path))
+            {
+                string fileinfo = reader.ReadToEnd();
+
+                reader.Close();
+                reader.Close();
+                OrderByFrequencyThenAlphabaticalOrder( fileinfo);
+                //WriteErrorLog(fileToLoad + "loaded");
+                //Console.WriteLine(fileToLoad + "loaded");
+
+            }
+        }
+
+        public static void OrderByFrequencyThenAlphabaticalOrder(string content)
+        {
+            List<string> list = content.Split(';').ToList();
+
+        }
+        public static void AddressesSortedAlphabeticallyByStreetName(string content)
+        {
+
+        }
+        public static void FrequencyFirstDescendingAndThenAlphabeticallyAscendingAlphabaticalOrder(string content)
+        {
+
+        }
+        public static void ListOfNames(string content)
+        {
+
+        }
+
+        public static void FirstList(string content)
+        {
+
+        }
+    }
+}
